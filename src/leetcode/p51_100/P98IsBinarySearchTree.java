664bd3ea057a65426eaae3f66842f5d78c41a96f
MichealJoker <407133142@qq.com>
2021-12-01 23:50:53 +0800

    leetcode98是否是平衡搜索二叉树

---
+++ src/leetcode/p51_100/P98IsBinarySearchTree.java
@@ -0,0 +1,58 @@
+package leetcode.p51_100;
+
+import jdk.nashorn.internal.objects.NativeUint8Array;
+import sun.reflect.generics.tree.Tree;
+
+/**
+ * @author: jzh
+ * @date: created in 2021/12/1
+ * @description: 是不是搜索二叉树  https://leetcode-cn.com/problems/validate-binary-search-tree/
+ * @version: 1.0
+ */
+public class P98IsBinarySearchTree {
+
+
+    public static class Info {
+        public boolean isBST;
+        public int max;
+        public int min;
+
+        public Info(boolean is, int ma, int mi) {
+            isBST = is;
+            max = ma;
+            min = mi;
+        }
+    }
+
+
+    public static Info process(TreeNode x) {
+        if (x == null) {
+            return null;
+        }
+        Info leftInfo = process(x.left);
+        Info rightInfo = process(x.right);
+        int max = x.val;
+        int min = x.val;
+        if (leftInfo != null) {
+            max = Math.max(leftInfo.max, max);
+            min = Math.min(leftInfo.min, min);
+        }
+        if (rightInfo != null) {
+            max = Math.max(rightInfo.max, max);
+            min = Math.min(rightInfo.min, min);
+        }
+        boolean isBST = true;
+        if (leftInfo != null && !leftInfo.isBST) {
+            isBST = false;
+        }
+        if (rightInfo != null && !rightInfo.isBST) {
+            isBST = false;
+        }
+        boolean leftMaxLessX = leftInfo == null ? true : (leftInfo.max < x.val);
+        boolean rightMinMoreX = rightInfo == null ? true : (rightInfo.min > x.val);
+        if (!leftMaxLessX || !rightMinMoreX) {
+            isBST = false;
+        }
+        return new Info(isBST, max, min);
+    }
+}
